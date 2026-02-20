99f28c188fde837998b313488382029ca1a0948d
sarah <sarah.mohammadian95@gmail.com>
2026-02-21 00:34:47 +0330

    feat: create separate user details page

--- src/features/dashboard/users/index.ts
+++ src/features/dashboard/users/index.ts
@@ -2,7 +2,6 @@
 export { UserSearch } from "./components/UserSearch";
 export { UserSkeleton } from "./components/UserSkeleton";
 export { UserTable } from "./components/UsersTable";
-export { UserDetailDrawer } from "./components/UserDetailDrawer";
 
 // Hooks
 export { useUsers } from "./hooks/useUsers";
